1aa126d7490e1e6a5ad6d1daef95a717c261cc7b
Fabián García <fabian.garcia@outlook.com>
2020-11-01 23:48:44 -0600

    Actividad: Reloj Digital Original: Muestra la hora. Novedad: Se agrego boton para ver en formato militar o convencional e icono. Por hacer: Comentar el codigo.

---
+++ RelojDigital/RelojDigital/Main.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RelojDigital
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblHora = new System.Windows.Forms.Label();
+            this.btnTipoHora = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHora.AutoSize = true;
+            this.lblHora.BackColor = System.Drawing.Color.Black;
+            this.lblHora.Font = new System.Drawing.Font("OCR A Extended", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.ForeColor = System.Drawing.Color.Lime;
+            this.lblHora.Location = new System.Drawing.Point(12, 9);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(486, 50);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "12 : 00 : 00 Hrs";
+            // 
+            // btnTipoHora
+            // 
+            this.btnTipoHora.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTipoHora.Location = new System.Drawing.Point(138, 75);
+            this.btnTipoHora.Name = "btnTipoHora";
+            this.btnTipoHora.Size = new System.Drawing.Size(235, 43);
+            this.btnTipoHora.TabIndex = 1;
+            this.btnTipoHora.Text = "Ver Hora Convencional";
+            this.btnTipoHora.UseVisualStyleBackColor = true;
+            this.btnTipoHora.Click += new System.EventHandler(this.btnTipoHora_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 130);
+            this.Controls.Add(this.btnTipoHora);
+            this.Controls.Add(this.lblHora);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Reloj Digital";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Button btnTipoHora;
+    }
+}
+
---
+++ RelojDigital/RelojDigital/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RelojDigital
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+            EstablecerHora(Properties.Settings.Default.TipoHora);
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void EstablecerHora(string tipoHora)
+        {
+            switch (tipoHora)
+            {
+                case "Ver Hora Convencional":
+                    this.Text = "Reloj Digital - Hora Militar";
+                    btnTipoHora.Text = "Ver Hora Convencional";
+                    lblHora.Text = $"{DateTime.Now.Hour} : {DateTime.Now.Minute} : {DateTime.Now.Second} Hrs";
+                    lblHora.Location = new Point(12, 9);                    
+                    break;
+                case "Ver Hora Militar":
+                    this.Text = "Reloj Digital - Hora Convencional";
+                    btnTipoHora.Text = "Ver Hora Militar";
+                    lblHora.Text = DateTime.Now.ToLongTimeString();
+                    lblHora.Left = (this.Width - lblHora.Width) / 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            EstablecerHora(btnTipoHora.Text);
+        }
+
+        private void btnTipoHora_Click(object sender, EventArgs e)
+        {
+            if (btnTipoHora.Text == "Ver Hora Convencional")
+                btnTipoHora.Text = "Ver Hora Militar";
+            else
+                btnTipoHora.Text = "Ver Hora Convencional";
+
+            EstablecerHora(btnTipoHora.Text);
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.TipoHora = (btnTipoHora.Text == "Ver Hora Convencional") ? "Ver Hora Convencional" : "Ver Hora Militar";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
